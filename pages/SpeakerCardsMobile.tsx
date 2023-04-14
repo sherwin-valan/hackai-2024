dceb1a6ee207a8a116628dc69c2d728fcbdd7ea7
vvyn <vicavyn3@gmail.com>
2023-04-14 16:08:06 -0500

    update speaker section

--- pages/SpeakerCardsMobile.tsx
+++ pages/SpeakerCardsMobile.tsx
@@ -4,29 +4,12 @@ import { useState } from 'react';
 function SpeakerCardsMobile()
 {
   return(
-   <div className="h-[1500px] grid grid-rows-3 justify-center place-content-center">
+   <div className="h-[700px] grid grid-rows-3 justify-center place-content-center">
       <div className="h-[500px] flex flex-col items-center">
-        <img src="/web1.png" className="w-[300px] h-[300px] object-cover rounded-[20px]"/>
+        <img src="/web1.jpg" className="w-[300px] h-[300px] object-cover rounded-[20px]"/>
         <div className="text-center font-sans">
-          <h2 className="text-[#1A2025] text-xl">Lokesh</h2>
-          <h2 className="text-[#1A2025] text-base">Tech Lead</h2>
-          <p className="text-base font-hind">Coming Soon</p>
-        </div>
-      </div>
-      <div className="h-[500px] w-[350px] pt-[20px] border-y-[2px] border-[#B1D4E0] flex flex-col items-center">
-        <img src="/web2.png" className="w-[300px] h-[300px] object-cover rounded-[20px]"/>
-        <div className="text-center font-sans">
-          <h2 className="text-[#1A2025] text-xl">Lokesh</h2>
-          <h2 className="text-[#1A2025] text-base">Tech Lead</h2>
-          <p className="text-base font-hind">Coming Soon</p>
-        </div>
-      </div>
-      <div className="h-[500px] mt-[20px] flex flex-col items-center">
-        <img src="/web3.png" className="w-[300px] h-[300px] object-cover rounded-[20px]"/>
-        <div className="text-center font-sans">
-          <h2 className="text-[#1A2025] text-xl">Lokesh</h2>
-          <h2 className="text-[#1A2025] text-base">Tech Lead</h2>
-          <p className="text-base font-hind">Coming Soon</p>
+          <h2 className="text-[#1A2025] text-xl">Guhan Venguswamy</h2>
+          <h2 className="text-[#1A2025] text-base">Head of Platform at Jasper.ai</h2>
         </div>
       </div>
    </div>
--- pages/SpeakersSection.tsx
+++ pages/SpeakersSection.tsx
@@ -11,14 +11,8 @@ function AboutSection()
     return (
         <div className="relative text-black">
             <img className="w-screen h-[1000px]" src="/speakers-bg.png" alt="speakers background image"/>
-            { mobile ?
-                <>
-                    <img className="w-screen h-[1000px]" src="/speakers-bg.png" alt="speakers background image"/>
-                    <img className="absolute w-screen h-[1000px] translate-y-[-1500px] object-cover" src="/speaker-blend.svg"/>
-                </>
-            : <></> }
             <div className="absolute w-full pt-2.5 top-[20vh] text-xl text-center leading-4 justify-center place-content-center sm:px-8 md:px-28 lg:px-40">
-                <Typography fontFamily={"var(--lucidity)"} className="pb-14 text-3xl">MEET OUR SPEAKERS</Typography>
+                <Typography fontFamily={"var(--lucidity)"} className="pt-24 pb-16 text-4xl">MEET OUR SPEAKERS</Typography>
                 {mobile ? <SpeakerCardsMobile/> : <SpeakerCards/>}
             </div>
         </div>
